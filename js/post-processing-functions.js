df3fc83326235e55bc74b49c06e0cf33cb572431
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-08-07 20:59:15 -0400

    update

--- js/post-processing-functions.js
+++
@@ -1,5 +0,0 @@
-// import jquery-1.7.1
-document.write('<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>');
-
-// import `utils.js`
-document.write('<script type="text/javascript" src="./js/.utils.js"></script>');
--- js/pre-processing-functions.js
+++ js/pre-processing-functions.js
@@ -1,2 +1,5 @@
-/** import `utils.js` */ // $.getScript("./js/utils.js");
+// import jquery-1.7.1
+document.write('<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>');
+
+// import `utils.js`
 document.write('<script type="text/javascript" src="./js/utils.js"></script>');
