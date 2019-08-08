7205c46e5c49a7aa4d2e31b556cfb2f288c35cb6
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-08-08 16:18:35 -0400

    update

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,5 @@
 // Utility JS functions are to be defined here
+function resizeIFrameToFitContent( iFrame ) {
+    iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
+    iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
+}
