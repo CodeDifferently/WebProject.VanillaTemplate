090bca86a3a9a6fc6da4dd25019043dcd69fdc07
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-08-07 22:11:33 -0400

    modularized `index.html`

--- js/pre-processing-functions.js
+++ js/pre-processing-functions.js
@@ -1,5 +1,5 @@
 // import jquery-1.7.1
-document.write('<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>');
+document.write('<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>');
 
 // import `utils.js`
 document.write('<script type="text/javascript" src="./js/utils.js"></script>');
