5fc0b99db141eed8eeae9cfe19debb57e2f6727b
git-leon <xleonhunter@gmail.com>
2019-10-15 22:40:06 -0400

    created a degree of modularity

--- js/file-inject.js
+++ js/file-inject.js
@@ -1,21 +1,4 @@
-function readFile(fileName) {
-    var result = load(fileName);
-    return result;
-}
-
-function injectFile(fileName) {
-    var textFileAsString = readFile(fileName);
-    console.log(getAllMethodsOfObejct(textFileAsString));
-    console.log(textFileAsString);
-    document.getElementsByTagName("body")[0].innerHTML += `${textFileAsString}`;
-};
 
 function loadHtml(elementId, fileName) {
-    $(elementId).load(fileName, function() {
-        alert( "Load was performed." );
-    });
-}
-
-
-loadHtml("output", "./test.txt");
-// injectFile("./test.txt");
+    $(elementId).html('<object data="'+ fileName + '">');
+};
